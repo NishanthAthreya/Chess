2d39497f37af8c31c09487362a11b4b70d0c9847
nis909 <nsa48@scarletmail.rutgers.edu>
2017-03-03 13:51:24 -0500

    updated knight

--- Knight.java
+++ Knight.java
@@ -1,1 +1,17 @@
-gvfbhg
+public class Knight extends Piece{
+	public Knight(){
+		
+	}
+
+	@Override
+	boolean canMove(String newLoc) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	void moveTo(String newLoc) {
+		// TODO Auto-generated method stub
+		
+	}
+}
