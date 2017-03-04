86cd6f14f3ab2181830113a6e96b7ba2d2a2cd0b
pkanukollu <pkanukollu>
2017-03-04 16:18:03 -0500

    adding bishop class

---
+++ chess/Bishop.java
@@ -0,0 +1,28 @@
+package chess;
+
+public class Bishop extends Piece{
+
+	private String color;
+	public Location location;
+	public Bishop(Location location, String color)
+	{
+		this.location = location;
+		this.color = color;
+	}
+	public boolean canMove(Location newLoc)
+	{
+		
+	}
+	public void moveTo(Location newLoc)
+	{
+		
+	}
+	public String getColor()
+	{
+		return color;
+	}
+	public String toString()
+	{
+		
+	}
+}
