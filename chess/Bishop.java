e775d46cb501a6c824208ce1b84d0b7cb209c997
pkanukollu <pkanukollu>
2017-03-05 15:19:38 -0500

    created bishop class

--- chess/Bishop.java
+++ chess/Bishop.java
@@ -9,13 +9,74 @@ public class Bishop extends Piece{
 		this.location = location;
 		this.color = color;
 	}
-	public boolean canMove(Location newLoc)
+	public boolean canMove(Location newLoc, Board b)
 	{
-		
+		int currFile = location.convertX();
+		int currRank = location.getY();
+		int newFile = newLoc.convertX();
+		int newRank = newLoc.getY();
+		int filediff = Math.abs(newFile-currFile);
+		int rankdiff = Math.abs(newRank-currRank);
+		if (filediff!=rankdiff)
+		{
+			return false;
+		}
+		if (newFile > currFile && newRank > currRank)	//topright
+		{
+			for (int i = currRank+1, j = currFile+1; i<=newRank; i++, j++)
+			{
+				if (b.board[i][j]!=null)
+				{
+					//System.out.println("topright");
+					return false;
+				}
+			}
+		}
+		if (newFile > currFile && newRank < currRank)	//bottomright
+		{
+			for (int i = currRank-1, j = currFile+1; j<=newFile; i--, j++)
+			{
+				if (b.board[i][j]!=null)
+				{
+					//System.out.println("bottomright");
+					return false;
+				}
+			}
+		}
+		if (newFile < currFile && newRank > currRank)	//topleft
+		{
+			for (int i = currRank+1, j = currFile-1; i<=newRank; i++, j--)
+			{
+				if (b.board[i][j]!=null)
+				{
+					//System.out.println("topleft" +  " " + b.board[i][j]);
+					return false;
+				}
+			}
+		}
+		if (newFile < currFile && newRank < currRank)	//bottomleft
+		{
+			for (int i = currRank-1, j = currFile-1; i>=newRank; i--, j--)
+			{
+				if (b.board[i][j]!=null)
+				{
+					//System.out.println("bottomleft");
+					return false;
+				}
+			}
+		}
+		return true;
 	}
-	public void moveTo(Location newLoc)
+	public boolean moveTo(Location newLoc, Board b)
 	{
-		
+		if (this.canMove(newLoc, b))
+		{
+			location = newLoc;
+			return true;
+		}
+		System.out.println("Illegal move, try again");
+		System.out.println();
+		return false;
 	}
 	public String getColor()
 	{
@@ -23,6 +84,10 @@ public class Bishop extends Piece{
 	}
 	public String toString()
 	{
-		
+		if (color == "white")
+		{
+			return "wB";
+		}
+		return "bB";
 	}
 }
