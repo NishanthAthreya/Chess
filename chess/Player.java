598a97f9d70ce4c382162e8c6e24e721210e9771
nis909 <nsa48@scarletmail.rutgers.edu>
2017-03-09 16:01:24 -0500

    can now transform pawn to queen, bishop, knight, etc. when it reaches end of board

--- chess/Player.java
+++ chess/Player.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Scanner;
+
 public class Player {
 	private String color;
 	public Player(String color){
@@ -8,6 +10,8 @@ public class Player {
 	public boolean move(Board b, Location init, Location to){
 		boolean canMove = false;
 		Piece curr = b.board[init.getY()][init.convertX()];
+		@SuppressWarnings("resource")
+		Scanner keyboard = new Scanner(System.in);
 		if (b.board[to.getY()][to.convertX()]!=null)	//pi
 		{
 			Piece p = b.board[to.getY()][to.convertX()];
@@ -17,6 +21,30 @@ public class Player {
 				b.board[to.getY()][to.convertX()]=curr;
 				b.board[init.getY()][init.convertX()]=null;
 				//curr.moveTo(to);
+				if(to.getY() == 7){
+					System.out.println("Please enter what piece you want your pawn to"
+							+ "transform into");
+					String x = keyboard.nextLine().toUpperCase();
+					String color;
+					switch(x){
+					case "BISHOP":
+						color = b.board[to.getY()][to.convertX()].getColor();
+						b.board[to.getY()][to.convertX()] = new Bishop(to, color);
+						break;
+					case "KNIGHT":
+						color = b.board[to.getY()][to.convertX()].getColor();
+						b.board[to.getY()][to.convertX()] = new Knight(to, color);
+						break;
+					case "ROOK":
+						color = b.board[to.getY()][to.convertX()].getColor();
+						b.board[to.getY()][to.convertX()] = new Rook(to, color);
+						break;
+					default:
+						color = b.board[to.getY()][to.convertX()].getColor();
+						b.board[to.getY()][to.convertX()] = new Queen(to, color);
+						break;
+					}
+				}
 				return true;
 				}	
 			else{
@@ -32,6 +60,30 @@ public class Player {
 			b.board[to.getY()][to.convertX()]=curr;
 			b.board[init.getY()][init.convertX()]=null;
 		//curr.moveTo(to);
+			if(to.getY() == 7){
+				System.out.println("Please enter what piece you want your pawn to"
+						+ "transform into");
+				String x = keyboard.nextLine().toUpperCase();
+				String color;
+				switch(x){
+				case "BISHOP":
+					color = b.board[to.getY()][to.convertX()].getColor();
+					b.board[to.getY()][to.convertX()] = new Bishop(to, color);
+					break;
+				case "KNIGHT":
+					color = b.board[to.getY()][to.convertX()].getColor();
+					b.board[to.getY()][to.convertX()] = new Knight(to, color);
+					break;
+				case "ROOK":
+					color = b.board[to.getY()][to.convertX()].getColor();
+					b.board[to.getY()][to.convertX()] = new Rook(to, color);
+					break;
+				default:
+					color = b.board[to.getY()][to.convertX()].getColor();
+					b.board[to.getY()][to.convertX()] = new Queen(to, color);
+					break;
+				}
+			}
 			return true;
 		}
 		return false;
